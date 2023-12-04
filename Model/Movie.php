b8e9126393a28b730859314ca887edbb9df1699a
Eecodev <mnasser.bbk@gmail.com>
2023-12-04 16:47:25 +0100

    first commit

---
+++ Model/Movie.php
@@ -0,0 +1,23 @@
+<?php
+class Movie
+{
+    private int $id;
+    private string $title;
+    private string $overview;
+    private float $vote_average;
+    private string $poster_path;
+    private string $original_language;
+
+    function __construct($id, $title, $overview, $vote, $language, $image)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->overview = $overview;
+        $this->vote_average = $vote;
+        $this->poster_path = $image;
+        $this->original_language = $language;
+    }
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include __DIR__ ."/Views/header.php";
+include __DIR__ ."/Model/Movie.php";
+?>
+<section class="container">
+    <h2>Movies</h2>
+</section>
+<?php
+include __DIR__ ."/Views/footer.php";
+?>
